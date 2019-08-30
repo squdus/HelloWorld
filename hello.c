05e5c939c547c60098679f53da0b4f91be15e9ce
squdus <54606844+squdus@users.noreply.github.com>
2019-08-29 19:21:33 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+*Author: Sara Qudus, Claire Scorenson, Parul Aggarial, Hallie Starns
+*Date: 2019/08/29
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+printif("Sara Qudus\n");
+
+return 0;
+}
